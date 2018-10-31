0c66f7663cafac7843195e74b79f0e348cf9ff24
David Gracia <david.gracia.celemendi@gmail.com>
2018-10-31 16:20:08 +0100

    some tests refactored to clarify cases and reduce amount of work done on each test

--- src/test/java/com/dgraciac/mars_rover/test/RoverShould.java
+++ src/test/java/com/dgraciac/mars_rover/test/RoverShould.java
@@ -45,18 +45,16 @@ public class RoverShould {
     }
 
     @Test
-    public void be_at_1_1_E_when_grid_is_3_1_and_rover_starts_1_1_E_and_it_moves_EAST_until_wraps_around() {
+    public void be_at_1_1_E_when_grid_is_3_1_and_rover_starts_3_1_E_and_it_moves_EAST_and_wraps_around() {
         int width = 3;
         int height = 1;
         GridDimensions gridDimensions = new GridDimensions(width, height);
-        int x = 1;
+        int x = 3;
         int y = 1;
         Location initialLocation = new Location(x,y);
         Position initialPosition = Position.EAST;
         Rover rover = new Rover(gridDimensions, initialLocation, initialPosition);
 
-        rover.move();
-        rover.move();
         rover.move();
 
         assertThat(rover.location()).isEqualTo(new Location(1, 1));
@@ -64,18 +62,16 @@ public class RoverShould {
     }
 
     @Test
-    public void be_at_3_1_E_when_grid_is_3_1_and_rover_starts_3_1_W_and_it_moves_WEST_until_wraps_around() {
+    public void be_at_3_1_E_when_grid_is_3_1_and_rover_starts_1_1_W_and_it_moves_WEST_and_wraps_around() {
         int width = 3;
         int height = 1;
         GridDimensions gridDimensions = new GridDimensions(width, height);
-        int x = 3;
+        int x = 1;
         int y = 1;
         Location initialLocation = new Location(x,y);
         Position initialPosition = Position.WEST;
         Rover rover = new Rover(gridDimensions, initialLocation, initialPosition);
 
-        rover.move();
-        rover.move();
         rover.move();
 
         assertThat(rover.location()).isEqualTo(new Location(3, 1));
@@ -100,7 +96,7 @@ public class RoverShould {
     }
 
     @Test
-    public void be_at_1_1_N_when_grid_is_1_3_and_rover_starts_1_3_N_and_it_moves_1_square_NORTH() {
+    public void be_at_1_1_N_when_grid_is_1_3_and_rover_starts_1_3_N_and_it_moves_1_square_NORTH_and_wraps_around() {
         int width = 1;
         int height = 3;
         GridDimensions gridDimensions = new GridDimensions(width, height);
@@ -117,18 +113,16 @@ public class RoverShould {
     }
 
     @Test
-    public void be_at_1_3_S_when_grid_is_1_3_and_rover_starts_1_3_S_and_it_moves_SOUTH_until_wraps_around() {
+    public void be_at_1_3_S_when_grid_is_1_3_and_rover_starts_1_1_S_and_it_moves_SOUTH_and_wraps_around() {
         int width = 1;
         int height = 3;
         GridDimensions gridDimensions = new GridDimensions(width, height);
         int x = 1;
-        int y = 3;
+        int y = 1;
         Location initialLocation = new Location(x,y);
         Position initialPosition = Position.SOUTH;
         Rover rover = new Rover(gridDimensions, initialLocation, initialPosition);
 
-        rover.move();
-        rover.move();
         rover.move();
 
         assertThat(rover.location()).isEqualTo(new Location(1, 3));
