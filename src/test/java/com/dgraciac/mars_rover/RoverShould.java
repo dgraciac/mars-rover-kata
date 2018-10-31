2f9bc3147b65b984b3f960cf05cdaa8a64c911b1
David Gracia <david.gracia.celemendi@gmail.com>
2018-10-31 13:12:39 +0100

    refactor: Location.java

--- src/test/java/com/dgraciac/mars_rover/RoverShould.java
+++ src/test/java/com/dgraciac/mars_rover/RoverShould.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RoverShould {
     @Test
-    public void be_at_2_1_E_when_grid_is_3_1_and_rover_starts_1_1_E_it_and_moves_1_square_EAST() {
+    public void be_at_2_1_E_when_grid_is_3_1_and_rover_starts_1_1_E_and_it_moves_1_square_EAST() {
         int width = 3;
         int height = 1;
         Grid grid = new Grid(width, height);
@@ -21,5 +21,23 @@ public class RoverShould {
         assertThat(rover.location()).isEqualTo(new Location(2, 1));
         assertThat(rover.position()).isEqualTo(Position.EAST);
     }
+
+/*    @Test
+    public void be_at_3_1_E_when_grid_is_3_1_and_rover_starts_1_1_E_and_it_moves_2_squares_EAST() {
+        int width = 3;
+        int height = 1;
+        Grid grid = new Grid(width, height);
+        int x = 1;
+        int y = 1;
+        Location initialLocation = new Location(x,y);
+        Position initialPosition = Position.EAST;
+        Rover rover = new Rover(grid, initialLocation, initialPosition);
+
+        rover.move();
+        rover.move();
+
+        assertThat(rover.location()).isEqualTo(new Location(3, 1));
+        assertThat(rover.position()).isEqualTo(Position.EAST);
+    }*/
 
 }
